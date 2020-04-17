19151c858b7cb9e32e2ad8ec1213b40204244346
Benedikt Werner <1benediktwerner@gmail.com>
2020-04-17 04:21:07 +0200

    Updates

---
+++ precache-manifest.81988f19018d14376a0f23303d0a3017.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "803fc2f6ec02ccaa1233bc947a52ab2b",
+    "url": "/dnd-tracker/index.html"
+  },
+  {
+    "revision": "7e81c089ff87b2b00e6e",
+    "url": "/dnd-tracker/static/css/main.bf15d514.chunk.css"
+  },
+  {
+    "revision": "49991e1812109f9a3b5a",
+    "url": "/dnd-tracker/static/js/2.ce29475e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dnd-tracker/static/js/2.ce29475e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e81c089ff87b2b00e6e",
+    "url": "/dnd-tracker/static/js/main.7f35ce61.chunk.js"
+  },
+  {
+    "revision": "fea5e66fc092e6e9de26",
+    "url": "/dnd-tracker/static/js/runtime-main.686b6e75.js"
+  }
+]);
